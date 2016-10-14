8ee3fdbf1270d8678dc4bf95179a863024f7788c
Dmitrijs Milajevs <dimazest@gmail.com>
2016-10-14 20:52:31 +0000

    Cleanup

--- not_followed.sql
+++
@@ -1,5 +0,0 @@
-select lower(mention#>>'{}') as m, count(*)
-from tweet, jsonb_array_elements(features->'user_mentions') mention
-where lower(mention#>>'{}') not like '@%'
-group by m
-order by count(*) desc;
